551dff3c0ce48bb939618a076bf315d3ea617a0b
Viktor Mirzoyan <44904186+vmirz@users.noreply.github.com>
2025-04-18 13:09:55 -0400

    Default to kaspr v0.5.10

--- kaspr/types/models/version_resources.py
+++ kaspr/types/models/version_resources.py
@@ -23,11 +23,18 @@ class KasprVersionResources:
     # TODO: This should be moved to a configuration file
     _VERSIONS = (        
         KasprVersion(
+            operator_version="0.5.8",
+            version="0.5.10",
+            image="kasprio/kaspr:0.5.10-alpha",
+            supported=True,
+            default=True,
+        ),
+        KasprVersion(
             operator_version="0.5.5",
             version="0.5.9",
             image="kasprio/kaspr:0.5.9-alpha",
             supported=True,
-            default=True,
+            default=False,
         ),        
         KasprVersion(
             operator_version="0.4.1",
