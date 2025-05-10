8474efff1be03e1f549f059305e3fe649fb0dbac
Viktor Mirzoyan <44904186+vmirz@users.noreply.github.com>
2025-05-10 16:33:21 -0400

    Bump to kaspr v0.5.13

--- kaspr/types/models/version_resources.py
+++ kaspr/types/models/version_resources.py
@@ -21,13 +21,20 @@ class KasprVersion(NamedTuple):
 class KasprVersionResources:
     #: Mapping of operator version to kaspr application version
     # TODO: This should be moved to a configuration file
-    _VERSIONS = (        
+    _VERSIONS = (
+        KasprVersion(
+            operator_version="0.5.14",
+            version="0.5.14",
+            image="kasprio/kaspr:0.5.13-alpha",
+            supported=True,
+            default=True,
+        ),    
         KasprVersion(
             operator_version="0.5.13",
             version="0.5.13",
             image="kasprio/kaspr:0.5.12-alpha",
             supported=True,
-            default=True,
+            default=False,
         ),          
         KasprVersion(
             operator_version="0.5.11",
