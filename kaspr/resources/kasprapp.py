e60e1a081d9f87f3e881b9d4817ca2983b963418
Viktor Mirzoyan <44904186+vmirz@users.noreply.github.com>
2025-09-23 21:10:52 -0400

    Bump version to 0.7.5 and update HPA scaling policies
    * Updated 0.7.5.
    * Adjusted HPA scaling policy periods and stabilization window in KasprApp.

--- kaspr/resources/kasprapp.py
+++ kaspr/resources/kasprapp.py
@@ -1161,16 +1161,16 @@ class KasprApp(BaseResource):
                             V2HPAScalingPolicy(
                                 type="Percent",
                                 value=100,
-                                period_seconds=60,
+                                period_seconds=90,
                             ),
                             V2HPAScalingPolicy(
                                 type="Pods",
                                 value=4,
-                                period_seconds=60,
+                                period_seconds=90,
                             ),
                         ],
                         select_policy="Max",
-                        stabilization_window_seconds=0,
+                        stabilization_window_seconds=30,
                     ),
                     scale_down=V2HPAScalingRules(
                         policies=[
