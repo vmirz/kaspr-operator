971de63c2902cbed470914a56ae894758fb81965
Viktor Mirzoyan <44904186+vmirz@users.noreply.github.com>
2025-11-04 13:13:46 -0500

    Bump version to 0.9.5 and update version resources
    Updated __version__ to 0.9.5 in kaspr/__init__.py. Added new KasprVersion entry for operator version 0.9.5 and set it as default in version_resources.py; previous default (0.9.4) is now non-default.

--- kaspr/types/models/version_resources.py
+++ kaspr/types/models/version_resources.py
@@ -23,11 +23,18 @@ class KasprVersionResources:
     # TODO: This should be moved to a configuration file
     _VERSIONS = (
         KasprVersion(
+            operator_version="0.9.5",
+            version="0.7.4",
+            image="kasprio/kaspr:0.7.4-alpha",
+            supported=True,
+            default=True,
+        ),
+        KasprVersion(
             operator_version="0.9.4",
             version="0.7.3",
             image="kasprio/kaspr:0.7.3-alpha",
             supported=True,
-            default=True,
+            default=False,
         ),    
         KasprVersion(
             operator_version="0.9.3",
